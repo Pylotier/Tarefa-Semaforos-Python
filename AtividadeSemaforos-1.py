24bbf8ec53f84626856994d15304c0430180362b
Pylotier <marcosmario2011@gmail.com>
2026-05-20 19:27:46 -0300

    Update AtividadeSemaforos-1.py

--- AtividadeSemaforos-1.py
+++ AtividadeSemaforos-1.py
@@ -1,6 +1,3 @@
-# 1. Fazer uma aplicação, console, que gerencie a figura abaixo: 
-# Para tal, usar uma variável sentido, que será alterado pela Thread que controla cada carro com a movimentação do carro. Quando a Thread tiver a possibilidade de ser executada, ela deve imprimir em console o sentido que o carro está passando. Só pode passar um carro por vez no cruzamento.
-
 import multiprocessing
 import time
 
