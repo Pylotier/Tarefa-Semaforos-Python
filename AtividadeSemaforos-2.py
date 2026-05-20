d3ad4fc42e069857a3800d05c12a53f8287dd7bd
Pylotier <marcosmario2011@gmail.com>
2026-05-20 19:27:33 -0300

    Update AtividadeSemaforos-2.py

--- AtividadeSemaforos-2.py
+++ AtividadeSemaforos-2.py
@@ -1,8 +1,3 @@
-#Quatro pessoas caminham, cada uma em um corredor diferente. Os 4 corredores terminam
-#em uma única porta. Apenas 1 pessoa pode cruzar a porta, por vez. Considere que cada
-#corredor tem 200m. e cada pessoa anda de 4 a 6 m/s. Cada pessoa leva de 1 a 2 segundos
-#para abrir e cruzar a porta. Faça uma aplicação que simule essa situação.
-
 import multiprocessing
 import time
 import random
